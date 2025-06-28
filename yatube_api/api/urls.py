dbafd09d0c14231f141918ccf3eb167e3ada55a4
Тимур Даянов <tima.skye@yandex.ru>
2025-06-28 21:53:18 +0500

    переписал урлы через роутеры

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,12 +1,18 @@
-from django.urls import path
+from django.urls import path, include
 from rest_framework.authtoken import views
+from rest_framework.routers import SimpleRouter
+from rest_framework_nested.routers import NestedSimpleRouter
 
+from api.views import PostViewSet, GroupsViewSet, CommentViewSet
+
+router = SimpleRouter()
+router.register('posts', PostViewSet)
+router.register('groups', GroupsViewSet)
+
+posts_router = NestedSimpleRouter(router, r'posts', lookup='post')
+posts_router.register(r'comments', CommentViewSet, basename='post-comments')
 urlpatterns = [
-    path('api-token-auth/',views.obtain_auth_token),
-    path('posts/',...),
-    path('posts/<int:post_id>/',...),
-    path('groups/',...),
-    path('groups/<int:group_id>/',...),
-    path('posts/<int:post_id>/comments/',...),
-    path('posts/<int:post_id>/comments/<int:comment_id>/',...),
+    path('api-token-auth/', views.obtain_auth_token),
+    path('', include(router.urls)),
+    path('', include(posts_router.urls)),
 ]
