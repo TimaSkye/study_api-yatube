97dce6e79601c6bbe3493995aae0173b76f143ad
Тимур Даянов <tima.skye@yandex.ru>
2025-06-28 21:18:15 +0500

    переписал урлы

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
+from rest_framework.authtoken import views
 
 urlpatterns = [
-    path('api-token-auth/',...),
-    path('posts/',...),
-    path('posts/<int:post_id>/',...),
-    path('groups/',...),
-    path('groups/<int:group_id>/',...),
-    path('posts/<int:post_id>/comments/',...),
-    path('posts/<int:post_id>/comments/<int:comment_id>/',...),
+    path('v1/api-token-auth/',views.obtain_auth_token),
+    path('v1/posts/',...),
+    path('v1/posts/<int:post_id>/',...),
+    path('v1/groups/',...),
+    path('v1/groups/<int:group_id>/',...),
+    path('v1/posts/<int:post_id>/comments/',...),
+    path('v1/posts/<int:post_id>/comments/<int:comment_id>/',...),
 ]
