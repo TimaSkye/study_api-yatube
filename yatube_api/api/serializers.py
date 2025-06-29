ae11d2665a4781261b2492dabe1ccd295c22ab20
Тимур Даянов <tima.skye@yandex.ru>
2025-06-29 18:09:59 +0500

    Проработал сериализаторы

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -1,20 +1,44 @@
 from rest_framework import serializers
+from posts.models import Post, Group, Comment
 
-from posts.models import Post
+
+class GroupSerializer(serializers.ModelSerializer):
+    """Сериализатор модели Group."""
+
+    class Meta:
+        model = Group
+        fields = '__all__'
 
 
 class PostSerializer(serializers.ModelSerializer):
-    author = serializers.PrimaryKeyRelatedField(read_only=True)
-    group = serializers.PrimaryKeyRelatedField(read_only=True)
+    """
+    Сериализатор для модели Post.
+    Поле author доступно только для чтения.
+    Поле group сериализуется по slug.
+    """
+    author = serializers.StringRelatedField(read_only=True)
+    group = serializers.SlugRelatedField(
+        slug_field='slug',
+        queryset=Group.objects.all(),
+        required=False,
+        allow_null=True
+    )
 
     class Meta:
         model = Post
-        fields = ('id', 'text', 'pub_date', 'author', 'image', 'group')
+        fields = '__all__'
+        read_only_fields = ('author',)
 
 
-class GroupSerializer:
-    pass
+class CommentSerializer(serializers.ModelSerializer):
+    """
+    Сериализатор для модели Comment.
+    Поля author, post и created доступны только для чтения.
+    """
+    author = serializers.StringRelatedField(read_only=True)
+    post = serializers.PrimaryKeyRelatedField(read_only=True)
 
-
-class CommentSerializer:
-    pass
+    class Meta:
+        model = Comment
+        fields = '__all__'
+        read_only_fields = ('author', 'post', 'created')
